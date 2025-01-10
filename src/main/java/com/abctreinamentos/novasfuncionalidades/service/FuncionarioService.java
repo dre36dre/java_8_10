4174c3d9393bcc6d5f804c5e8b3364644a6b0304
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2025-01-10 01:06:12 +0000

    Java 9 default method

--- src/main/java/com/abctreinamentos/novasfuncionalidades/service/FuncionarioService.java
+++ src/main/java/com/abctreinamentos/novasfuncionalidades/service/FuncionarioService.java
@@ -8,4 +8,31 @@ public interface FuncionarioService {
 
 	List<Funcionario> listAll();
 	
+	default void printAll(){
+		List<Funcionario> funcionarios= listAll();
+		init();
+		funcionarios.forEach(System.out::print);
+	}
+
+	static void calcularBonus(Funcionario funcionario)
+	{
+		List<Double> lista=List.of(0.1,0.15,0.2);
+
+		double bonus = 0;
+
+		if(funcionario.getSalario() <=5000  )
+		bonus=funcionario.getSalario()*lista.get(2);
+		else if(funcionario.getSalario()  <= 6000)
+		bonus=funcionario.getSalario() *lista.get(1);
+		else
+		bonus=funcionario.getSalario()*lista.get(0);
+
+		System.out.println(bonus);
+
+	}
+
+	private void init()
+	{
+		System.out.println("Listando todos os funcionarios da aplicação");
+	}
 }
