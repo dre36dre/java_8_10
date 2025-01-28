a3c9d1c8342c956ee2f7fe9d9e7aab4b6a639e72
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2025-01-28 01:27:47 +0000

    Calculating the value of hours

--- src/main/java/com/Algorithms/src/Travel.java
+++ src/main/java/com/Algorithms/src/Travel.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class Travel {
+    public static void main(String[] args) {
     
+    int day=0;
+    int hours=0;
+    int totalHours;
+    Scanner in= new Scanner(System.in);
+    System.out.println("Please inform the number of days of the trip:");
+    day=in.nextInt();
+    System.out.println("Please inform the number of hours of the trip");
+        hours=in.nextInt();
+        totalHours=day *24+hours;
+
+        System.out.println("The total value of the trip hours in hours was: "+totalHours);
+        in.close();
+    }
 }
