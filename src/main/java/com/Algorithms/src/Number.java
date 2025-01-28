89474f4af2578b35838a74865e0395cca329966e
Anderson Freires <55098476+dre36dre@users.noreply.github.com>
2025-01-28 01:41:04 +0000

    Reporting the previous and next number

---
+++ src/main/java/com/Algorithms/src/Number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Number {
+    public void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int number, previous,later;
+
+        System.out.println("Please enter a number");
+        number=in.nextInt();
+        previous=number+1;
+        later=number-1;
+        System.out.println("previous number"+previous);
+        System.out.println("Later number"+later);
+        
+
+
+
+
+    }
+    
+}
